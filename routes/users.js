5947e5645bc78492826ee74498ab57dd0ad15ffc
pamindakw <pamindaya@gmail.com>
2019-02-26 09:48:02 +0530

    Node-CRUD Login Panel

---
+++ routes/users.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+/* GET users listing. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Node-Crud' });
+});
+
+//db authenticate
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud"
+});
+
+con.connect(function(err) {
+    if (err) {
+      console.log("Error connecting!");
+      return;
+    }
+      console.log("Connected!");
+  });
+
+router.get('/', function(req, res_, next) {
+  var username = req.query['name'];
+  var password = req.query['pass'];
+
+  if (username && password) {
+    con.query('SELECT * FROM login WHERE username = ? AND password = ?', [username, password], function(err, res) {
+      if (res.length > 0) {
+        req.session.loggedin = true;
+        req.session.username = username;
+        res_.redirect('/');
+      } else {
+        res_.send('Incorrect Username and/or Password!');
+      }
+    });
+  } else {
+    req.session.loggedin = false;
+    req.session.username = username;
+    res_.send('Please enter Username and Password!');
+  }
+});
+
+module.exports = router;
